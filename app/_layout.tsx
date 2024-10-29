5e378660f75b51509ee17c1cd3727641c201723b
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-10-30 01:35:47 +0500

    Feat: Drag and drop animation implemented using reanimated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "expo-router";
+import React from "react";
 import { LogBox, StatusBar } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" />
       <Stack>
         <Stack.Screen
@@ -19,6 +21,6 @@ export default function RootLayout() {
 
         <Stack.Screen name="+not-found" />
       </Stack>
-    </>
+    </GestureHandlerRootView>
   );
 }
